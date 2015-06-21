bd9347cfdc42a05215962e41fff9d607e2c4f2d3
josh <jalexy12@gmail.com>
2015-06-21 12:22:50 -0400

    Adding logic to moving pieces around the table in the terminal

--- lib/Game.rb
+++ lib/Game.rb
@@ -1,38 +1,57 @@
 require_relative 'Board.rb'
+require 'awesome_print'
+require 'pry'
+require_relative 'CoordSplitter'
 
 class Game
+	include CoordSplitter
+
 	def initialize
 		@board = Board.new
+		@rows = [[],[],[],[],[],[],[],[]]
 		@table =  Terminal::Table.new
 		make_rows
-		print @table
+		# print @table
 	end
 
 	def make_rows
-		rows = [[],[],[],[],[],[],[],[]]
 		alpha_proc = lambda { |letter, comparison|  letter.to_s.split("")[0] == comparison }
 		letter_counter = 0
 
 		("a".."h").each do |letter|
 			@board.board.each do |key, value|
 				if alpha_proc.call(key, letter)
-					rows[letter_counter].push([key.to_s, value.class.to_s])
+					@rows[letter_counter].push([key.to_s, value.class.to_s])
 				end
 			end
 			letter_counter += 1
 		end
-		@table.rows = rows
+		@table.rows = @rows
 	end
 
 	def validate
 		moves = IO.read("chessMoves.txt").split("\n")
 
-		new_moves = moves.map do | move |
-			move.split
-		end
+		moves.each do | move |
+			new_move = move.split
+			@board.move!(new_move[0], new_move[1])
+		end	
+		print @table
+	end
 
-		new_moves.each do |move|
-		 	@board.move!(move[0], move[1])
-		 end
+	def update_table(original, final)
+		x =  cord_splitter(original)[0] - 1
+		y =  cord_splitter(original)[1] - 1	
+		new_x = cord_splitter(final)[0] - 1
+		new_y = cord_splitter(final)[1] - 1
+		piece = @rows[x][y][1]
+
+		
+		@rows[x][y] = [original, nil]
+		@rows[new_x][new_y] = [final, piece]
+
+		@table.rows = @rows
+		print @table
+	 
 	end
 end
--- newChess.rb
+++ newChess.rb
@@ -3,18 +3,25 @@ require 'terminal-table'
 require 'awesome_print'
 require_relative './lib/Game.rb'
 
-moves = IO.read("chessMoves.txt").split("\n")
 
-new_moves = moves.map do | move |
-	move.split
-end
 
 
 game = Game.new
 
 
 
-game.validate
+# game.validate
+moves = IO.read("chessMoves.txt").split("\n")
 
+# moves.each do | move |
+# 	new_move = move.split
+# 	# @board.move!(new_move[0], new_move[1])
+# 	game.update_table(new_move[0], new_move[1], "King")
+# end	
+# game.update_table()
+
+game.update_table("a1", "a4")
+
+# game.validate
 
 
